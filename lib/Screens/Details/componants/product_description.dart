38c02c4a15f1a21c52b73c4a00e972f25c5457f2
Ankur <ankur12591@gmail.com>
2021-05-07 17:23:23 +0530

    commit

---
+++ lib/Screens/Details/componants/product_description.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:furniture_app/Components/title_text.dart';
+import 'package:furniture_app/models/Product.dart';
+
+import '../../../constants.dart';
+import '../../../size_config.dart';
+
+class ProductDescription extends StatelessWidget {
+  final Product product;
+
+  const ProductDescription({this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(SizeConfig.defaultSize * 2), //20
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(SizeConfig.defaultSize * 1.5), //20
+            child: TitleText(
+              title: product.subTitle,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(SizeConfig.defaultSize * 1.5), //20
+            child: Text(
+              product.description,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(SizeConfig.defaultSize * 1.5), //20
+            width: SizeConfig.screenWidth,
+            height: 70,
+            child: FlatButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)),
+              color: kPrimaryColor,
+              onPressed: () {},
+              child: Text(
+                "Add to Cart",
+                style: TextStyle(
+                  fontSize: SizeConfig.defaultSize * 1.8,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/Screens/Details/details_screen.dart
+++ lib/Screens/Details/details_screen.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:furniture_app/Screens/Details/componants/product_description.dart';
+import 'package:furniture_app/models/Product.dart';
+
+import '../../constants.dart';
+import '../../size_config.dart';
 
 class DetailsScreen extends StatefulWidget {
+  final Product product;
+
+  const DetailsScreen({this.product});
+
   @override
   _DetailsScreenState createState() => _DetailsScreenState();
 }
@@ -9,8 +19,40 @@ class _DetailsScreenState extends State<DetailsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-
+      appBar: AppBar(
+        leading: IconButton(
+          icon: SvgPicture.asset(
+            "assets/icons/arrow-long-left.svg",
+            // height: SizeConfig.defaultSize * 2,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Text(""),
+        actions: [
+          IconButton(
+              icon: SvgPicture.asset(
+                "assets/icons/bag.svg",
+                color: kTextColor,
+                // height: SizeConfig.defaultSize * 2.4,
+              ),
+              onPressed: () {}),
+          SizedBox(
+            width: SizeConfig.defaultSize,
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              ProductDescription(
+                product: product,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
